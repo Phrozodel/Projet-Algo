bfbcb82ecdbed52eb313744028fab860e2ce3621
Phrozodel <96947558+Phrozodel@users.noreply.github.com>
2022-02-10 00:08:57 +0100

    Update Gestion_guilde.ads

--- Gestion_guilde.ads
+++ Gestion_guilde.ads
@@ -1,1 +1,33 @@
+WITH Ada.Text_IO,Ada.Integer_Text_IO,Outils;
+USE Ada.Text_IO,Ada.Integer_Text_IO,Outils;
 
+PACKAGE  Gestion_Guildes IS
+
+   TYPE T_CellG;
+   TYPE T_PtG IS ACCESS T_CellG;-- pointeur Guilde
+   TYPE T_CellG IS
+      RECORD
+         Guilde : T_Guilde;
+         Suiv   : T_Ptg;
+      END RECORD;
+   TYPE T_FileG IS
+      RECORD
+         Teteg,
+         Fing  : T_Ptg;
+      END RECORD;
+
+   PROCEDURE Enfiler_Guilde (
+         F : IN OUT T_Fileg;
+         G : IN     T_Guilde);
+   PROCEDURE Newguilde (
+         G :    OUT T_Guilde);
+   PROCEDURE Dissolution (
+         F :        T_Ptg;
+         G : IN OUT T_Guilde);
+   PROCEDURE Newguilde_Coach (
+         G      : IN OUT T_Guilde;
+         C      : IN OUT T_Coach;
+         Erreur :    OUT Boolean);
+   PROCEDURE VisuG (
+         F : IN     T_Ptg);
+END Gestion_Guildes;
