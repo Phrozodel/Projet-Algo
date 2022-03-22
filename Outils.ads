c5f1d78a5d353f0fafdb94e4f767d982337a9f9b
Phrozodel <96947558+Phrozodel@users.noreply.github.com>
2022-03-22 19:51:34 +0100

    Update Outils.ads

--- Outils.ads
+++ Outils.ads
@@ -14,35 +14,20 @@ subtype t_mot is string (1..30);
          PointsG    : Integer := 4;
                   Dissoute: boolean:=false;
       END RECORD;
-   TYPE T_Expertise IS
-         (Novice,
-          Confirme,
-          Expert,
-          Maitre);
-   TYPE T_Coach IS
-      RECORD
-         NomC      : T_Mot       := (OTHERS => ' ');
+      
+
+
+TYPE T_Expertise IS (Novice, Confirme, Expert,  Maitre);
+
+   TYPE T_Coach IS RECORD
+         NomC      : T_Mot:= (OTHERS  => ' ');
          Expertise : T_Expertise := Novice;
-         Guilde    : T_Guilde;
-         Nb_Pokes  : Integer     := 0;               --Inferieur a 14
-         PointsC   : Integer;
-                  sansguilde: boolean:=true;
+         Guilde    : T_Mot;
+         Nb_Pokes  : Integer:= 0; --Inferieur a 14
+         PointsC   : Integer:=6;
+         sansguilde: boolean:=true;
       END RECORD;
 
-
-   TYPE T_CategorieP IS
-         (Eau,
-          Feu,
-          Terre,
-          Air);
-
-   TYPE T_Poke IS
-      RECORD
-         NomP  : T_Mot   := (OTHERS => ' ');          --Unique PV : Integer := 6;
-         Force : Integer := 2;
-         Coach : T_Coach ;
-                  Orphelin: boolean:=true;
-      END RECORD;
-
+  
 Procedure rien; --juste pour que le body soit accepte pour l'instant
 END Outils;
