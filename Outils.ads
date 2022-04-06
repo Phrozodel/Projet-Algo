780c69290879b0b79c9a3e4ce9b697cc4a8b2cba
SednaRaffaud <99345659+SednaRaffaud@users.noreply.github.com>
2022-04-06 10:10:19 +0200

    Update Outils.ads

--- Outils.ads
+++ Outils.ads
@@ -31,3 +31,44 @@ TYPE T_Expertise IS (Novice, Confirme, Expert,  Maitre);
   
 Procedure rien; --juste pour que le body soit accepte pour l'instant
 END Outils;
+
+
+
+
+
+
+--------- nouvelle version sedna -------------
+
+
+WITH Ada.Text_IO,Ada.Integer_Text_IO;
+USE Ada.Text_IO,Ada.Integer_Text_IO;
+
+PACKAGE  Outils IS
+
+subtype T_Mot is string (1..30);
+
+
+      TYPE T_Guilde IS RECORD
+         NomG       : T_Mot   := (OTHERS => ' ');
+         Nb_Membres : Integer := 0;
+         PointsG    : Integer := 4;
+      END RECORD;
+
+ TYPE T_Expertise IS (Novice, Confirme, Expert,  Maitre);
+
+
+TYPE T_Coach IS RECORD
+         NomC      : T_Mot:= (OTHERS  => ' ');
+         Expertise : T_Expertise := Novice;
+         Guilde    : T_Mot:= (OTHERS  => ' ');
+         Nb_Pokes  : Integer:= 0; --Inferieur a 14
+         PointsC   : Integer:=6;
+         sansguilde: boolean:=true;
+END RECORD;
+
+
+
+   PROCEDURE Rien; --juste pour que le body soit accepte pour l'instant
+
+      END Outils;
+
